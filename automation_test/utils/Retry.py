8319fe34bdca16ce1c0519190fa1da9f79702fe8
guoli5 <guoli5@jd.com>
2016-06-27 16:19:57 +0800

    add docker deamon port and add docker version

--- automation_test/utils/Retry.py
+++ automation_test/utils/Retry.py
@@ -14,15 +14,15 @@ container_daemon = conf_obj['docker_daemon']
 def init():
     return SQLClient(conf_obj['mysql_host'], conf_obj["mysql_port"], conf_obj["mysql_user"],conf_obj["mysql_passwd"],conf_obj["mysql_db"])
 
-def retry_get_new_slave(space_id,slave_port):
+def retry_get_new_slave(space_id,slave_ip,slave_port):
     slave_ip_new = 0
     slave_port_new = 0
     sql_c = init()
-    while 1==1:
+    while True:
         slave_info = sql_c.get_slave_ip_port(space_id)
         slave_ip_new = slave_info[0]
         slave_port_new = slave_info[1]
-        if (slave_port_new != slave_port) and (slave_port_new != 0) :
+        if (slave_port_new != 0) and (slave_ip_new != 0) and (not ((slave_ip == slave_ip_new) and (slave_port ==  slave_port_new))):
             print "[INFO] {0}:{1} is created by failover. Failover can recreate new master for cache instance (space_id={2})".format(slave_ip_new,slave_port_new,space_id)
             break
         print "[INFO] Failover is recreating master for instance (space_id={0}) ...".format(space_id)
@@ -30,15 +30,15 @@ def retry_get_new_slave(space_id,slave_port):
 
     return slave_port_new
 
-def retry_get_new_master(space_id,master_port):
+def retry_get_new_master(space_id,master_ip,master_port):
     master_ip_new = 0
     master_port_new = 0
     sql_c = init()
-    while 1==1:
+    while True:
         master_info = sql_c.get_master_ip_port(space_id)
         master_ip_new = master_info[0]
         master_port_new = master_info[1]
-        if (master_port_new != master_port) and (master_port_new != 0) :
+        if (master_port_new != 0) and (master_ip_new != 0) and (not ((master_ip == master_ip_new) and (master_port ==  master_port_new))):
             print "[INFO] {0}:{1} is created by failover. Failover can recreate new master for cache instance (space_id={2})".format(master_ip_new,master_port_new,space_id)
             break
         print "[INFO] Failover is recreating master for instance (space_id={0}) ...".format(space_id)
