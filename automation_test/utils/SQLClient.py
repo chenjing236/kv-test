e75b0af48bb85c7c53e7776553c1e220860ae621
guoli5 <guoli5@jd.com>
2016-07-18 10:15:30 +0800

    space id is changed to string

--- automation_test/utils/SQLClient.py
+++ automation_test/utils/SQLClient.py
@@ -30,7 +30,7 @@ class SQLClient(object):
         '''
         print ", space_id[{0}]".format(space_id)
         self.init_cursor()
-        sql = "select status,capacity,password,flag,tenant_id,name,remarks from space where id={0}".format(space_id)
+        sql = "select status,capacity,password,flag,tenant_id,name,remarks from space where space_id='{0}'".format(space_id)
         n = self.cursor.execute(sql)
         if n < 1:
             return None
@@ -48,7 +48,7 @@ class SQLClient(object):
         '''
         print "begin to get instances, space_id:[{0}]".format(space_id)
         self.init_cursor()
-        sql = "select ip,port,copy_id,flag from instance where space_id={0}".format(space_id)
+        sql = "select ip,port,copy_id,flag from instance where space_id='{0}'".format(space_id)
         n = self.cursor.execute(sql)
         if n < 1:
             return None
@@ -67,7 +67,7 @@ class SQLClient(object):
 
     def get_acl(self, space_id):
         self.init_cursor()
-        sql = "select src_ip,tenant_id from acl where space_id={0}".format(space_id)
+        sql = "select src_ip,tenant_id from acl where space_id='{0}'".format(space_id)
         self.cursor.execute(sql)
         ips = self.cursor.fetchall()
         acl_ip = []
@@ -84,7 +84,7 @@ class SQLClient(object):
 
     def get_domain(self, space_id):
         self.init_cursor()
-        sql = "select domain from ap where space_id={0}".format(space_id)
+        sql = "select domain from ap where space_id='{0}'".format(space_id)
         n = self.cursor.execute(sql)
         if n > 1:
             raise Exception("get > 1 record")
@@ -100,7 +100,7 @@ class SQLClient(object):
         :param space_id:
         :return: [slave(ip,port)]
         '''
-        self.sql_slave = "select ip,port from instance where space_id={0} and copy_id!='m'".format(space_id)
+        self.sql_slave = "select ip,port from instance where space_id='{0}' and copy_id!='m'".format(space_id)
         n = self.run(self.sql_slave)
         slave_ip = 0
         slave_port = 0
@@ -118,7 +118,7 @@ class SQLClient(object):
         :param space_id:
         :return: [master(ip,port)]
         '''
-        self.sql_master = "select ip,port from instance where space_id={0} and copy_id='m'".format(space_id)
+        self.sql_master = "select ip,port from instance where space_id='{0}' and copy_id='m'".format(space_id)
         n = self.run(self.sql_master)
         master_ip = 0
         master_port = 0
@@ -132,7 +132,7 @@ class SQLClient(object):
 
     # 查询topology表中的is_locked字段，is_locked=0表示缓存云实例没有被锁住，is_locked=1表示缓存云实例被锁住，返回值为-1表示没有在topology表中找到对应的数据
     def is_locked(self, space_id):
-        self.sql_is_locked = "select is_locked from topology where space_id={0}".format(space_id)
+        self.sql_is_locked = "select is_locked from topology where space_id='{0}'".format(space_id)
         n = self.run(self.sql_is_locked)
         if n > 0:
             is_locked = self.cursor.fetchone()
@@ -142,7 +142,7 @@ class SQLClient(object):
         return -1
 
     def get_epoch(self,space_id):
-        self.sql_epoch = "select epoch from topology where space_id={0}".format(space_id)
+        self.sql_epoch = "select epoch from topology where space_id='{0}'".format(space_id)
         n = self.run(self.sql_epoch)
         if n > 0:
             epoch = self.cursor.fetchone()
