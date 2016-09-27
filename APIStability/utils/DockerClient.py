749eb73260aebf19c0318ac2e8a3e3bc82ecee76
zhangpengyun <zhangpengyun@jd.com>
2016-09-27 16:38:57 +0800

    update jmiss stability test case

--- APIStability/utils/DockerClient.py
+++ APIStability/utils/DockerClient.py
@@ -13,6 +13,7 @@ class DockerClient(object):
     def inspect_container(self, container_host, container_port):
         base_url = "tcp://{0}:{1}".format(container_host, self.docker_daemon_port)
         container_name = "{0}_{1}".format(container_host, container_port)
+<<<<<<< HEAD
         info_logger.info("check container memsize: begin to check container:{0}".format(container_name))
         c = Client(base_url=base_url, version=self.docker_version)
         ret_containers = c.containers(filters={"name": container_name}, all=True)
@@ -22,13 +23,30 @@ class DockerClient(object):
         info_logger.debug("check container memsize: containers info={0}".format(json.dumps(ret_containers[0])))
         inspect = c.inspect_container(container_name)
         info_logger.debug("check container memsize: inspect_container info={0}".format(json.dumps(inspect)))
+=======
+        info_logger.info("check redis memsize: begin to check container:{0}".format(container_name))
+        c = Client(base_url=base_url, version=self.docker_version)
+        ret_containers = c.containers(filters={"name": container_name}, all=True)
+        if len(ret_containers) < 1:
+            info_logger.error("check redis memsize: can't find container:[{0}]".format(container_name))
+            return False
+        info_logger.debug("check redis memsize: containers info={0}".format(json.dumps(ret_containers[0])))
+        inspect = c.inspect_container(container_name)
+        info_logger.debug("check redis memsize: inspect_container info={0}".format(json.dumps(inspect)))
+>>>>>>> update jmiss stability test case
 
         # check redis memory
         redis_memsize = inspect['Args'][3]
         cmd_memsize = inspect['Config']['Cmd'][3]
         info_logger.info("redis_memsize={0}, cmd_memsize={1}".format(redis_memsize, cmd_memsize))
         if redis_memsize != cmd_memsize:
+<<<<<<< HEAD
             info_logger.error("check container memsize: check container memsize error!")
             return False
         info_logger.info("check container memsize: check container memsize success!")
+=======
+            info_logger.error("check redis memsize: check redis memsize error!")
+            return False
+        info_logger.info("check redis memsize: check redis memsize success!")
+>>>>>>> update jmiss stability test case
         return True
