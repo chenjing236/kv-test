f2b62b34a0af42acb45a9b1e2f6a111d03002254
guoli5 <guoli5@jd.com>
2016-11-10 19:25:57 +0800

    update

--- jmiss_automation_test/test_cases/BasicTestCase.py
+++ jmiss_automation_test/test_cases/BasicTestCase.py
@@ -15,26 +15,30 @@ import json
 
 from utils.HttpClient import *
 from utils.RedisClient import *
+from utils.util import *
 from business_function.Cluster import *
 from business_function.Container import *
 from business_function.CFS import *
-from test_cases.ClusterOperation import *
+from steps.ClusterOperation import *
 
 def pytest_addoption(parser):
-    parser.addoption("--config_file", action="store", default="conf.json", help="test config file path")
-    parser.addoption("--instance_data", action="store", default="instance_data.json", help="data file path")
+    parser.addoption("--config", action="store", default="conf.json", help="test config file path")
+    parser.addoption("--data", action="store", default="instance_data.json", help="data file path")
 
-@pytest.fixture(scope="session")
+@pytest.fixture(scope="session", autouse=True)
 def config(request):
-    file_path = request.config.getoption("config_file")
+    #file_path = request.config.getoption("config")
     #file_path = "C:/Users/guoli5/git/JCacheTest/jmiss_automation_test/config/conf.json"
+    file_path = cur_file_dir().replace("\\","/").replace("test_cases", "") + "config/conf.json"
+    print "[INFO] Current path is {0}".format(file_path)
     conf_obj = json.load(open(file_path, 'r'))
     return conf_obj
 
 @pytest.fixture(scope="session")
 def instance_data(request):
-    file_path = request.config.getoption("instance_data")
+    #file_path = request.config.getoption("--data")
     #file_path="C:/Users/guoli5/git/JCacheTest/jmiss_automation_test/data/instance_data.json"
+    file_path = cur_file_dir().replace("\\","/").replace("test_cases", "") + "data/instance_data.json"
     data = json.load(open(file_path, 'r'))
     return data
 
