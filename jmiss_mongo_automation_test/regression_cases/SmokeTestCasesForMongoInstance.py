0b54ffa5ca1139121bcf4203304a374da2ffb67e
guoli5 <guoli5@jd.com>
2017-06-10 09:57:22 +0800

    add test cases

--- jmiss_mongo_automation_test/regression_cases/SmokeTestCasesForMongoInstance.py
+++ jmiss_mongo_automation_test/regression_cases/SmokeTestCasesForMongoInstance.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python
 # coding:utf-8
 import pytest
-#import logging
-#from BasicTestCase import *
+import logging
+from BasicTestCase import *
 
-#info_logger = logging.getLogger(__name__)
+info_logger = logging.getLogger(__name__)
 
 class TestSmokeCasesForMongoInstance:
 
     @pytest.mark.smoke
     def test_create_mongo_instance(self):
-        #info_logger.info("[SCENARIO] Create a mongo instance that consists of a primary container, a secondary container and a hidden container")
-        print "[SCENARIO] Create a mongo instance that consists of a primary container, a secondary container and a hidden container"
+        info_logger.info("[SCENARIO] Create a mongo instance that consists of a primary container, a secondary container and a hidden container")
