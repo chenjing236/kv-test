ef87db35ccd0386a2027d892758e9608d9e7aeab
zhangpengyun <zhangpengyun@jd.com>
2017-06-16 16:27:54 +0800

    add steps

---
+++ jmiss_cap_automation_test/regression_cases/redis_cases/TestDiscountAndCoupon.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*- 
+
+from BasicTestCase import *
+
+
+class TestDiscountAndCoupon:
+
+    @pytest.mark.smoke
+    def test_discount_and_coupon(self, create_redis_instance):
+        info_logger.info("[Scenario] Create an instance for redis, the instance consists of a master and a slave")
+        redis_cap, cap, request_id, resource_id = create_redis_instance
+        info_logger.info("[INFO] Test create redis instance successfully, the resourceId is {0}".format(resource_id))
