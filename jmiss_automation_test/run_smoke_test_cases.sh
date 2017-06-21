62d7de1709325a9e448ed8046a4b5a54e9f585b3
root <root@ym.gslb.qianxun.com>
2017-06-21 21:58:14 +0800

    add scripts for run smoke test cases

---
+++ jmiss_automation_test/run_smoke_test_cases.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+if [ $# -lt 1 ];then
+        echo "[WARNING] Please input sleep time"
+        exit 0
+fi
+# current path
+current_path=`pwd`
+wait_time=$1
+# list of test cases
+test_cases_list=`ls -l ${current_path}/test_cases | grep 'SmokeTestCases' | awk '{print $NF}'`
+for line in ${test_cases_list}
+do
+	python -m pytest -v ./regression_cases/redis_cases/$line -s -v --config ./config/redis_config/config_test.json --data ./data/redis_data/data_test.json
+	sleep ${wait_time}
+done
