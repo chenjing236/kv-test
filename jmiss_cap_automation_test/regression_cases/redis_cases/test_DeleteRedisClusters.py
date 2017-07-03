c4e0cba3be71c83c22026d334db5b6f7ac9c6a8d
zhangpengyun <zhangpengyun@jd.com>
2017-07-03 18:32:38 +0800

    delete create_redis_instance_no_teardown, fix case

--- jmiss_cap_automation_test/regression_cases/redis_cases/test_DeleteRedisClusters.py
+++ jmiss_cap_automation_test/regression_cases/redis_cases/test_DeleteRedisClusters.py
@@ -6,21 +6,42 @@ from BasicTestCase import *
 class TestDeleteCacheClusters:
 
     @pytest.mark.smoke
-    def test_delete_cache_clusters(self, create_redis_instance_no_teardown):
-        #创建主从版按配置redis资源
-        info_logger.info("[Scenario] Create an instance for redis, the instance consists of a master and a slave")
-        redis_cap, cap, request_id, resource_id = create_redis_instance_no_teardown
-        info_logger.info("[INFO] Test create redis instance successfully, the resourceId is {0}".format(resource_id))
-        #创建集群版按配置redis资源
-        info_logger.info("[Scenario] Create an instance for redis, the instance consists of a master and a slave")
-        redis_cap_2, cap_2, request_id_2, resource_id_2 = create_redis_instance_no_teardown
-        info_logger.info("[INFO] Test create redis instance successfully, the resourceId is {0}".format(resource_id))
-        #调用批量删除接口
-        cluster_ids =[request_id,resource_id_2]
+    def test_delete_cache_clusters(self, config, instance_data, redis_http_client, cap_http_client):
+        info_logger.info("[INFO] Test Delete clusters!")
+        redis_cap = RedisCap(config, instance_data, redis_http_client)
+        cap = Cap(config, instance_data, cap_http_client)
+        # 创建第一个redis实例
+        info_logger.info("[STEP] Create the first redis instance")
+        request_id_for_redis = create_redis_instance_step(redis_cap)
+        # 支付
+        info_logger.info("[STEP] Pay for the create order of redis instance")
+        pay_for_redis_instance_step(cap, request_id_for_redis)
+        # 查询订单状态
+        info_logger.info("[STEP] Query order status, check the status of order")
+        success, resource_id = query_order_status_step(cap, request_id_for_redis)
+        # 查询详情接口
+        info_logger.info("[STEP] Query redis instance detail, check the status of redis instance")
+        billing_order, cluster = query_cache_cluster_detail_step(redis_cap, resource_id)
+        assert cluster["status"] == 100, "[ERROR] The status of redis cluster is not 100!"
+        info_logger.info("[INFO] Create the first redis instance successfully, the resourceId is {0}".format(resource_id))
+
+        # 创建第二个redis资源
+        info_logger.info("[Scenario] Create the second redis instance")
+        request_id_for_redis = create_redis_instance_step(redis_cap)
+        # 支付
+        info_logger.info("[STEP] Pay for the create order of redis instance")
+        pay_for_redis_instance_step(cap, request_id_for_redis)
+        # 查询订单状态
+        info_logger.info("[STEP] Query order status, check the status of order")
+        success, resource_id_2 = query_order_status_step(cap, request_id_for_redis)
+        # 查询详情接口
+        info_logger.info("[STEP] Query redis instance detail, check the status of redis instance")
+        billing_order, cluster = query_cache_cluster_detail_step(redis_cap, resource_id)
+        assert cluster["status"] == 100, "[ERROR] The status of redis cluster is not 100!"
+        info_logger.info("[INFO] Create the second redis instance successfully, the resourceId is {0}".format(resource_id))
+        # 调用批量删除接口
+        info_logger.info("[STEP] Delete the two clusters now")
+        cluster_ids = [resource_id, resource_id_2]
         delete_cache_clusters_step(redis_cap, cluster_ids)
-
-        #验证返回信息正确
-
+        # 验证返回信息正确
         info_logger.info("[INFO] Test delete cacheClusters successfully!")
-
-
