273ff91e6f6814de9a90d63b6df0e1cabad61bc9
root <root@ym.gslb.qianxun.com>
2017-07-19 19:17:27 +0800

    add failover test cases

--- jmiss_mongo_automation_test/regression_cases/mongo_cases/test_failover_of_mongo_container.py
+++ jmiss_mongo_automation_test/regression_cases/mongo_cases/test_failover_of_mongo_container.py
@@ -10,10 +10,12 @@ class TestFailoverOfMongoInstance:
     # 创建mongo实例，验证stop mongo三个containers触发failover，failover执行完成后，mongo的状态为可用状态，副本集关系为primary, sencondary和hidden
     def _failover_mongo_containers(self, create_mongo_instance):
 	info_logger.info("[SCENARIO] Failover the primary container, the secondary container and the hidden container for the mongo instance")
+	# 通过space_id查看数据库instance表获取hidden的container id
+	# 调用nova api执行stop操作
+	# 通过space_id查看数据库scaler_task中task_type=3的记录，获取return_code
 	# 获取mongo的副本集关系
-	# 同时stop mongo实例的三个containers,触发failover
-	# 获取failover的操作结果，即查看failover是否完成，及完成的状态
-	# 获取mongo的副本集关系
+        # 验证return_code=0成功
+        # 验证拓扑信息中的hidden信息不为空
 
     def _failover_mongo_container_for_deleted_container(self, create_mongo_instance):
 	info_logger.info("[SCENARIO] Create new container for deleted container of the mongo instance")
