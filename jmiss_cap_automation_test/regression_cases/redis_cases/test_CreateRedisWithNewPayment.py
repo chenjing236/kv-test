3ce5d61ed5d5657e0067ddd966de94db642dab62
wangguilin7 <wangguilin@jd.com>
2017-09-05 16:05:28 +0800

    newpayment

--- jmiss_cap_automation_test/regression_cases/redis_cases/test_CreateRedisWithNewPayment.py
+++ jmiss_cap_automation_test/regression_cases/redis_cases/test_CreateRedisWithNewPayment.py
@@ -3,10 +3,10 @@
 from BasicTestCase import *
 
 
-class TestCreateRedisSkipPay:
+class TestCreateRedisWithNewPayment:
 
     @pytest.mark.smoke
-    def test_create_an_instance_skip_pay(self, create_redis_instance_skip_pay):
+    def test_create_an_instance_with_new_payment(self, create_redis_month_instance_with_new_payment):
         info_logger.info("[Scenario] Create an instance skipping paying for redis, the instance consists of a master and a slave")
-        redis_cap, cap, request_id, resource_id = create_redis_instance_skip_pay
+        redis_cap, cap, request_id, resource_id = create_redis_month_instance_with_new_payment
         info_logger.info("[INFO] Test create redis instance skipping paying successfully, the resourceId is {0}".format(resource_id))
