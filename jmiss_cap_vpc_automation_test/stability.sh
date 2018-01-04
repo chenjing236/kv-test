7b7e76a6c777c3f199f5335079dacf822c3fdd6a
zhangpengyun <zhangpengyun@jd.com>
2018-01-04 18:16:23 +0800

    update scripts region

--- jmiss_cap_vpc_automation_test/stability.sh
+++ jmiss_cap_vpc_automation_test/stability.sh
@@ -3,7 +3,7 @@ if [ $# -lt 1 ]; then
 	echo "ERROR: Parameter wrong"
         exit 1
 fi
-cd /export/Data/jmiss_auto_scripts/redis_stability_scripts/JCacheTest/jmiss_cap_vpc_automation_test
+cd /export/Data/jmiss_auto_scripts/redis_stability_scripts_$1/JCacheTest/jmiss_cap_vpc_automation_test
 
 export PYTHONPATH=`pwd`
 /usr/bin/python ./regression_cases/APIStabilityOfCap.py $1
