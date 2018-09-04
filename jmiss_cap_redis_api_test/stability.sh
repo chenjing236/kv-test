aa75c0311c6ff6bdc8286b4a3fd90a632b2377f9
张鹏云 <zhangpengyun@ZBMAC-C02VM6KYH.local>
2018-09-04 16:55:40 +0800

    fix stability.sh

--- jmiss_cap_redis_api_test/stability.sh
+++ jmiss_cap_redis_api_test/stability.sh
@@ -5,5 +5,5 @@ if [ $# -lt 1 ]; then
 fi
 cd /export/Data/jmiss_auto_scripts/redis_stability_scripts_$1/JCacheTest/jmiss_cap_redis_api_test
 
-export PYTHONPATH=`pwd`
+export PYTHONPATH=`pwd`:/export/Data/jmiss_auto_scripts/redis_stability_scripts_$1/jcloud-sdk-python/
 /usr/bin/python ./regression_cases/APIStabilityOfCap.py $1
