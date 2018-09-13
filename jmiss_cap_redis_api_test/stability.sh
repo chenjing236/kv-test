67f1b571a7c7597373a07f978c0de02091c7a3be
张鹏云 <zhangpengyun@ZBMAC-C02VM6KYH.local>
2018-09-13 11:35:11 +0800

    fix

--- jmiss_cap_redis_api_test/stability.sh
+++ jmiss_cap_redis_api_test/stability.sh
@@ -8,4 +8,4 @@ cd /export/Data/jmiss_auto_scripts/redis_stability_scripts_$1/JCacheTest/jmiss_c
 export PYTHONPATH=`pwd`:/export/Data/jmiss_auto_scripts/redis_stability_scripts_$1/jcloud-sdk-python/
 date=`date '+%Y%m%d-%H%M%S'`
 /usr/bin/python ./regression_cases/APIStabilityOfCap.py $1 > /export/Logs/redis_stability_test/redis_stability_$date.log 2>&1
-cat nohup.out | tail -n 8
+cat /export/Logs/redis_stability_test/redis_stability_$date.log | tail -n 8
