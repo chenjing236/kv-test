efc3cf8e3f12984ccc879f240461740156e618ff
张鹏云 <zhangpengyun@ZBMAC-C02VM6KYH.local>
2018-09-28 17:10:42 +0800

    add delete log

--- jmiss_cap_redis_api_test/stability.sh
+++ jmiss_cap_redis_api_test/stability.sh
@@ -9,3 +9,8 @@ export PYTHONPATH=`pwd`:/export/Data/jmiss_auto_scripts/redis_stability_scripts_
 date=`date '+%Y%m%d-%H%M%S'`
 /usr/bin/python ./regression_cases/APIStabilityOfCap.py $1 > /export/Logs/redis_stability_test/redis_stability_$date.log 2>&1
 cat /export/Logs/redis_stability_test/redis_stability_$date.log | tail -n 8
+
+# clean up old log
+today=`date '+%Y%m%d-%H%M%S' | awk -F'-' '{print $1}'`
+cd /export/Logs/redis_stability_test/
+ls | grep redis_stability*log | awk -v today=$today -F'-|_' '{if($3 < today-1) {print}}' | xargs \rm -f
