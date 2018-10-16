4e6231d7c883219348e43c7e7a8e76477c25efc1
duhaixing <duhaixing@jd.com>
2018-10-16 17:49:00 +0800

    modify memcached stability script.

--- jmiss_memcached_vpc_automation_test/stability.sh
+++ jmiss_memcached_vpc_automation_test/stability.sh
@@ -6,23 +6,23 @@ date=`date '+%Y%m%d-%H%M%S'`
 pytest ./test_cases/openapi_cases -s -v -m openapi --config ./config/conf_hb.json --data ./data/instance_data_hb.json > /export/Logs/mc_stability_test/mc_stability_$date.log 2>&1
 
 cases=(
-'createInstance Failed'
-'describeInstance Failed'
-'describeInstanceNotFound Failed'
-'describeInstances Failed'
-'flushInstance Failed'
-'flushInstanceNotFound Failed'
-'modifyInstance Failed'
-'modifyInstanceWithoutAuth Failed'
-'modifyInstanceSpec Failed'
+'createInstance'
+'describeInstance'
+'describeInstanceNotFound'
+'describeInstances'
+'flushInstance'
+'flushInstanceNotFound'
+'modifyInstance'
+'modifyInstanceWithoutAuth'
+'modifyInstanceSpec'
 )
 re=`cat /export/Logs/mc_stability_test/mc_stability_${date}.log|grep -E 'PASSED|FAILED'`
 i=0
 for ca in ${re[@]}; do
         if [[ $ca =~ 'PASSED' ]]; then
-                echo "mc.stab$i.status:0"
+                echo "mc.${cases[$i]}.status:\"0\""
         else
-                echo "mc.stab$i.status:"${cases[$i]}
+                echo "mc.${cases[$i]}.status:failed"
         fi
         ((i++))
 done
