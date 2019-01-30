ae703f683484de7c5f898df6733bb5ba4ff265ce
xieyichen <xieyichen@jd.com>
2019-01-30 18:13:52 +0800

    fix

--- jmiss_memcached_vpc_automation_test/scripts/memcached_rw_data.sh
+++ jmiss_memcached_vpc_automation_test/scripts/memcached_rw_data.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-Cmd="curl -s -m 10 -X GET http://xb8avfbqa212.cn-north-1.jdcloud-api.net/api/memcached?action=heartbeat"
+Cmd="curl -s -S -m 10 -X GET http://xb8avfbqa212.cn-north-1.jdcloud-api.net/api/memcached?action=heartbeat"
 CheckString="Healthy"
 MaxTimes=3
 LogDir="/export/Logs/memcached_read_write"
@@ -17,12 +17,12 @@ function check()
 {
     retryTime=0
     while [ ${retryTime} -lt ${MaxTimes} ]; do
-        curlResult=`${Cmd}`
+        curlResult=$(${Cmd} 2>&1)
         cmdResult=$?
         if [[ ${cmdResult} -eq 0 ]]; then
             break
         else
-            writelog "${cmdResult}"
+            writelog "${curlResult}"
         fi
         ((retryTime++))
         sleep 10
@@ -30,12 +30,12 @@ function check()
     if [[ ${curlResult} =~ ${CheckString} && ${retryTime} -lt ${MaxTimes} ]]; then
         echo "status:\"0\""
         writelog "Heartbeat result: ${curlResult}"
-    elif [ ${retryTime} -eq 3]; then
-        echo "status:\"-1\",desc: Connection time out, error code: ${cmdResult}"
-        writelog "Connection time out, error code: ${cmdResult}"
+    elif [ ${retryTime} -eq 3 ]; then
+        echo "status:\"-1\",desc: Error: ${curlResult}"
+        writelog "Error: ${curlResult}"
     else
         echo "status:\"-1\",desc: ${curlResult}"
-        writelog "${curlResult}"
+        writelog "Heartbeat result: ${curlResult}"
     fi
 }
 
