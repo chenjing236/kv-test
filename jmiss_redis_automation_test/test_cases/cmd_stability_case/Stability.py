182749af5acec2a25fb029fa9493b9be056c30c8
张鹏云 <zhangpengyun@jd.com>
2019-03-01 11:19:05 +0800

    update

--- jmiss_redis_automation_test/test_cases/cmd_stability_case/Stability.py
+++ jmiss_redis_automation_test/test_cases/cmd_stability_case/Stability.py
@@ -22,81 +22,65 @@ class Stability:
     def __init__(self, config, exec_times=-1):
         self.config = config
         self.exec_times = int(exec_times)
+        self.executor = ThreadPoolExecutor(max_workers=config['thread_num'])  # 最大并发数，线程池大小
+        self.db_num = self.config['dbnum_start']  # 执行命令的db_num，256个db轮询执行
+        self.f_list = {}  # 记录线程列表及db_num的对应关系
+        self.p_list = []  # 记录线程列表
 
     # 执行case
-    def run_command(self, case, db, sec):
+    def run_command(self, db):
+        case = random.sample(cases, 1)[0]
+        run_time = random.randint(self.config['runtime_start'], self.config['runtime_end'])
         print "cases execute times: ", case_num
         case_num[case] += 1
         time.sleep(2)
-        globals()[case](host=self.config['host'], port=self.config['port'], db=db, password=self.config['password'], sec=sec)
+        globals()[case](host=self.config['host'], port=self.config['port'], db=db, password=self.config['password'], sec=run_time)
+
+    # new thread
+    def start_new_task(self):
+        # 当前被线程占用的db_num
+        db_arr = self.f_list.values()
+        # 如果有未执行完的线程占用此db，则跳过这个db
+        while self.db_num in db_arr:
+            self.db_num = (self.db_num + 1) % 256
+        future = self.executor.submit(self.run_command, self.db_num)
+        add = find_add(future)
+        # 获取地址失败，返回失败
+        if add == '':
+            return False
+        # 新建的任务插入记录列表
+        self.p_list.append(future)
+        self.f_list[add] = self.db_num
+        self.exec_times -= 1
+        # db_num ++
+        self.db_num = (self.db_num + 1) % 256
+        return True
 
     def stability_test(self):
-        # 最大并发数，线程池大小
-        executor = ThreadPoolExecutor(max_workers=self.config['thread_num'])
-        # 执行命令的db_num，256个db轮询执行
-        db_num = self.config['dbnum_start']
-        # 记录线程与db num对应关系的map(pid, db_num)
-        f_list = {}
-        # 记录线程列表
-        p_list = []
-
-        for i in range(self.config['thread_num']):
-            if self.exec_times == 0:
-                break
-            random_case = random.sample(cases, 1)[0]
-            run_time = random.randint(self.config['runtime_start'], self.config['runtime_end'])
-            future = executor.submit(self.run_command, random_case, db_num, run_time)
-            add = find_add(future)
-            # 获取地址失败，继续循环
-            if add == '':
-                continue
-            p_list.append(future)
-            f_list[add] = db_num
-            self.exec_times -= 1
-            # db_num ++
-            db_num = (db_num + 1) % 256
-
         while True:
-            hello = wait(p_list, return_when='FIRST_COMPLETED', timeout=2)
+            hello = wait(self.p_list, return_when='ALL_COMPLETED', timeout=3)
             # hello[0]为已完成任务列表, 在f_list中删除已完成任务
             # hello[1]为未完成任务列表
             for j in hello[0]:
-                p_list.remove(j)
-                del f_list[find_add(j)]
+                self.p_list.remove(j)
+                del self.f_list[find_add(j)]
 
             # 限制任务数不超过线程池大小
             if len(hello[1]) >= self.config['thread_num']:
                 continue
             else:
-                # 新建case执行，执行计数-1
-                if self.exec_times > 0:
-                    self.exec_times -= 1
                 # case执行完成，没有剩余任务，结束执行
-                elif self.exec_times == 0 and len(hello[1]) == 0:
-                    print "cases execute times: ", case_num
+                if self.exec_times == 0 and len(hello[1]) == 0:
+                    print "【FINISHED】cases execute times: ", case_num
                     break
                 # case执行完成，等待剩余任务执行完成
                 elif self.exec_times == 0 and len(hello[1]) != 0:
                     continue
+                # else self.exec_times > 0, 还未达到次数限制，继续执行
                 # else: self.exec_times < 0, 没有次数限制，新建case执行
-            # 生成随机执行case
-            random_case = random.sample(cases, 1)[0]
-            # 生成随机执行时长
-            run_time = random.randint(self.config['runtime_start'], self.config['runtime_end'])
-            future = executor.submit(self.run_command, random_case, db_num, run_time)
-            add = find_add(future)
-            # 获取地址失败，继续循环
-            if add == '':
-                continue
-            # 当前被线程占用的db_num
-            db_arr = f_list.values()
-            # 如果有未执行完的线程占用此db，则跳过这个db
-            while db_num in db_arr:
-                db_num = (db_num + 1) % 256
-            p_list.append(future)
-            f_list[add] = db_num
-            # db_num ++, 到达255后，重新从0开始循环
-            db_num = (db_num + 1) % 256
+
+            # 插入新的任务
+            self.start_new_task()
 
 
 def main():
