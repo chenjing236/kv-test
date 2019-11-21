5bfc4d5055faae59defb31dc30cd6b9ad26d3203
duhaixing <duhaixing@jd.com>
2019-11-21 17:40:42 +0800

    memcached add sh region.

---
+++ jmiss_memcached_vpc_automation_test/stability_sh.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+cd /export/Data/jmiss_auto_scripts/memcached_stability_scripts/kv-test/jmiss_memcached_vpc_automation_test
+
+date=`date '+%Y%m%d-%H%M%S'`
+pytest ./test_cases/openapi_cases -s -v -m openapi --config ./config/conf_sh.json --data ./data/instance_data_sh.json > /export/Logs/mc_stability_test/mc_stability_sh_$date.log 2>&1
+
+cases=(
+'createInstance'
+'describeInstance'
+'describeInstanceNotFound'
+'describeInstances'
+'flushInstance'
+'flushInstanceNotFound'
+'modifyInstance'
+'modifyInstanceWithoutAuth'
+'modifyInstanceSpec'
+)
+re=`cat /export/Logs/mc_stability_test/mc_stability_sh_${date}.log|grep -E 'PASSED|FAILED'`
+i=0
+for ca in ${re[@]}; do
+        if [[ $ca =~ 'PASSED' ]]; then
+                echo "mc.${cases[$i]}.status:\"0\""
+        else
+                if [ $i -eq 0 ]; then
+                    echo "mc.${cases[$i]}.status:\"failed\""
+                    break
+                fi
+                echo "mc.${cases[$i]}.status:\"failed\""
+        fi
+        ((i++))
+done
+
+#clean up old log
+cd /export/Logs/mc_stability_test
+day=`date -d "2 days ago" +%Y%m%d`
+if [ -f *${day}*.log ]; then
+        ls *${day}*.log |xargs \rm -f
+fi
+
+
