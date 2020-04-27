ca4d62292d2620a778f789d280792616166f7b45
huangjunhong <huangjunhong@jd.com>
2020-04-27 14:42:39 +0800

    save

---
+++ jmiss_redis_automation_test/test_cases/create_cases/create_standard_and_failover.py
@@ -0,0 +1,19 @@
+#!/bin/python
+# coding:utf-8
+
+# 挑个1G主从版本即可
+
+## 有密码实例
+# admin发生failover
+# 单个proxy发生failover
+# 单个Redis master 发生failover
+# 单个redis slave 发生failover
+# 所有节点同时发生failover
+
+
+## 无密码实例
+# admin发生failover
+# 单个proxy发生failover
+# 单个Redis master 发生failover
+# 单个redis slave 发生failover
+# 所有节点同时发生failover
