ad57b851a3944a2f3c71f1173b603f420ea3748b
unknown <chenjing236@360buyad.local>
2020-04-27 18:38:07 +0800

    Move intergrate tests about creating instance and resize instance

--- jmiss_redis_automation_test/test_cases/create_cases/create_standard.py
+++ jmiss_redis_automation_test/test_cases/create_cases/create_standard.py
@@ -1,5 +1,6 @@
 #!/bin/python
 # coding:utf-8
+from jmiss_redis_automation_test.steps.InstanceOperation import *
 
 # 验证1G主从 有密码实例
 # 验证2G主从 无密码实例
@@ -7,3 +8,15 @@
 # 验证8G主从
 # ...
 # 验证64G主从
+
+
+@pytest.mark.intergration
+@pytest.mark.createstandard
+def test_specified_standard_createCacheInstance(self, config, instance_data, expected_data):
+    instances = instance_data["create_standard_specified"]
+    print("Standard instances count is %s" % len(instances))
+    for i in range(len(instances)):
+        client, _, instance_id = create_validate_instance(config, instances[i], expected_data)
+
+        if instance_id is not None:
+            delete_instance(config, instance_id, client)
--- jmiss_redis_automation_test/test_cases/openapi_cases/test_modifyCacheInstanceClass.py
+++ jmiss_redis_automation_test/test_cases/openapi_cases/test_modifyCacheInstanceClass.py
@@ -1,4 +1,3 @@
-from jmiss_redis_automation_test.steps.InstanceOperation import *
 from jmiss_redis_automation_test.steps.FusionOpertation import *
 from jmiss_redis_automation_test.steps.Valification import *
 
@@ -9,7 +8,6 @@ class TestModifyCacheInstanceClass:
     @pytest.mark.openapi
     @pytest.mark.smoke
     @pytest.mark.regression
-    @pytest.mark.cjdebug
     def test_modifyCacheInstanceClass(self, init_instance, config):
         client, resp, instance_id = init_instance
         resp = reset_class(config, instance_id, config["change_data"]["cacheInstanceClass"], client)
@@ -17,42 +15,4 @@ class TestModifyCacheInstanceClass:
         instance = query_instance_recurrent(300, 6, instance_id, config, client)
         assert instance["cacheInstanceClass"] == config["change_data"]["cacheInstanceClass"]
 
-    @pytest.mark.intergration
-    def test_standard_expansion(self, config, instance_data, expected_data):
-        modify_instance = instance_data["modify_standard_instance"]
-        client, _, instance_id = create_validate_instance(config, modify_instance, expected_data)
-        resp = reset_validate_class(config, instance_id, modify_instance["target_cacheInstanceClass"], client,
-                                    modify_instance["target_shardNumber"])
 
-        if instance_id is not None:
-            delete_instance(config, instance_id, client)
-
-    #@pytest.mark.intergration
-    def test_cluster_expansion(self, config, instance_data, expected_data):
-        modify_instance = instance_data["modify_cluster_instance"]
-        client, _, instance_id = create_validate_instance(config, modify_instance, expected_data)
-        resp = reset_validate_class(config, instance_id, modify_instance["target_cacheInstanceClass"], client,
-                                    modify_instance["target_shardNumber"])
-
-        if instance_id is not None:
-            delete_instance(config, instance_id, client)
-
-    #@pytest.mark.intergration
-    def test_standard_to_cluster(self, config, instance_data, expected_data):
-        modify_instance = instance_data["modify_standard_to_cluster_instance"]
-        client, _, instance_id = create_validate_instance(config, modify_instance, expected_data)
-        resp = reset_validate_class(config, instance_id, modify_instance["target_cacheInstanceClass"], client,
-                                    modify_instance["target_shardNumber"])
-
-        if instance_id is not None:
-            delete_instance(config, instance_id, client)
-
-    #@pytest.mark.intergration
-    def test_cluster_to_standard(self, config, instance_data, expected_data):
-        modify_instance = instance_data["modify_cluster_to_standard_instance"]
-        client, _, instance_id = create_validate_instance(config, modify_instance, expected_data)
-        resp = reset_validate_class(config, instance_id, modify_instance["target_cacheInstanceClass"], client,
-                                    modify_instance["target_shardNumber"])
-
-        if instance_id is not None:
-            delete_instance(config, instance_id, client)
