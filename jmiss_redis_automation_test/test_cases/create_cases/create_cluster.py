b3ee140f9e4450af9f0afd629304f1cd759812e2
unknown <chenjing236@360buyad.local>
2020-04-29 15:27:25 +0800

    Add intergrate tests about creating instances

--- jmiss_redis_automation_test/test_cases/create_cases/create_cluster.py
+++ jmiss_redis_automation_test/test_cases/create_cases/create_cluster.py
@@ -9,14 +9,16 @@ from jmiss_redis_automation_test.steps.InstanceOperation import *
 # 2T集群
 
 
-@pytest.mark.intergration
-@pytest.mark.createcluster
-def test_specified_cluster_createCacheInstance(self, config, instance_data, expected_data):
-    instances = instance_data["create_cluster_specified"]
-    print("Cluster instances count is %s" % len(instances))
-    for i in range(len(instances)):
-        client, _, instance_id = create_validate_instance(config, instances[i], expected_data)
+class TestCreateClusterIntergration:
 
-        if instance_id is not None:
-            delete_instance(config, instance_id, client)
+    @pytest.mark.intergration
+    @pytest.mark.createcluster
+    def test_specified_cluster_createCacheInstance(self, config, instance_data, expected_data):
+        instances = instance_data["create_cluster_specified"]
+        print("Cluster instances count is %s" % len(instances))
+        for i in range(len(instances)):
+            client, _, instance_id = create_validate_instance(config, instances[i], expected_data)
+
+            if instance_id is not None:
+                delete_instance(config, instance_id, client)
 
--- jmiss_redis_automation_test/test_cases/create_cases/create_cluster_and_failover.py
+++ jmiss_redis_automation_test/test_cases/create_cases/create_cluster_and_failover.py
@@ -1,27 +1,310 @@
 #!/bin/python
 # coding:utf-8
+import time
 
-# 挑个1G主从版本即可
-
-## 有密码实例
-# admin发生failover
-# 单个proxy发生failover
-# 单个Redis master 发生failover
-# 单个redis slave 发生failover
-# 1个redis-master、redis-slave 同时发生failover
-# 1个proxy、redis-master 同时发生failover
-# 1个proxy、redis-slave 同时发生failover
-# 1个proxy、redis-master、redis-slave同时发生failover
-# 所有节点同时发生failover
+from jmiss_redis_automation_test.steps.InstanceOperation import *
+from jmiss_redis_automation_test.steps.FailoverOperation import *
+from jmiss_redis_automation_test.utils.util import *
 
 
-## 无密码实例
-# admin发生failover
-# 单个proxy发生failover
-# 单个Redis master 发生failover
-# 单个redis slave 发生failover
-# 1个redis-master、redis-slave 同时发生failover
-# 1个proxy、redis-master 同时发生failover
-# 1个proxy、redis-slave 同时发生failover
-# 1个proxy、redis-master、redis-slave同时发生failover
-# 所有节点同时发生failover
+class TestCreateClusterFailover:
+
+    # admin发生failover
+    @staticmethod
+    def __admin_failover_validation(config, instance, expected_object, instance_id):
+        # admin failover trigger
+        replicasetName = instance_id + "-admin"
+        dockerName = replicasetName + "-0"
+        oldRunTime = get_docker_running_time(config, instance_id, replicasetName, dockerName)
+        status = trigger_docker_failover("admin", config, instance_id, config["region"])
+        assert status == 200
+        assert wait_docker_run_time_change(config, instance_id, oldRunTime, replicasetName, dockerName)
+
+        # base validation
+        assert check_admin_proxy_redis_configmap(instance_id, config, expected_object, instance["shardNumber"])
+
+    # 单个proxy发生failover
+    @staticmethod
+    def __proxy_failover_validation(config, expected_object, instance_id):
+        # proxy failover trigger
+        proxyId = get_shard_id(get_proxy_num(instance_id, config), 1)[0]
+        replicasetName = instance_id + "-proxy"
+        dockerName = replicasetName + "-" + str(proxyId)
+        oldRunTime = get_docker_running_time(config, instance_id, replicasetName, dockerName)
+        status = trigger_docker_failover("proxy", config, instance_id, config["region"], id=proxyId)
+        assert status == 200
+        assert wait_docker_run_time_change(config, instance_id, oldRunTime, replicasetName, dockerName)
+
+        # base validation
+        assert check_admin_proxy_configmap(instance_id, config, expected_object)
+
+    # 单个Redis master 发生failover
+    @staticmethod
+    def __master_failover_validation(config, instance, expected_object, instance_id):
+        # master failover trigger
+        current_rs_type = get_current_rs_type(instance_id, config)
+        redisNum = get_redis_num(instance_id, config, current_rs_type)
+        redisId = get_shard_id(redisNum, 1)[0]
+        replicasetName = instance_id + "-master-" + current_rs_type
+        dockerName = replicasetName + "-" + str(redisId)
+        oldRunTime = get_docker_running_time(config, instance_id, replicasetName, dockerName)
+        status = trigger_docker_failover("redis", config, instance_id, config["region"], docker_name=dockerName)
+        assert status == 200
+        assert wait_docker_run_time_change(config, instance_id, oldRunTime, replicasetName, dockerName)
+
+        time.sleep(30)
+        # base validation
+        assert check_admin_proxy_redis_configmap(instance_id, config, expected_object, instance["shardNumber"])
+
+    # 单个redis slave 发生failover
+    @staticmethod
+    def __slave_failover_validation(config, instance, expected_object, instance_id):
+        # slave failover trigger
+        current_rs_type = get_current_rs_type(instance_id, config)
+        redisNum = get_redis_num(instance_id, config, current_rs_type)
+        redisId = get_shard_id(redisNum, 1)[0]
+        replicasetName = instance_id + "-slave-" + current_rs_type
+        dockerName = replicasetName + "-" + str(redisId)
+        oldRunTime = get_docker_running_time(config, instance_id, replicasetName, dockerName)
+        status = trigger_docker_failover("redis", config, instance_id, config["region"], docker_name=dockerName)
+        assert status == 200
+        assert wait_docker_run_time_change(config, instance_id, oldRunTime, replicasetName, dockerName)
+
+        # base validation
+        assert check_admin_proxy_redis_configmap(instance_id, config, expected_object, instance["shardNumber"])
+
+    # 1个redis-master、redis-slave 同时发生failover
+    @staticmethod
+    def _slave_master_failover_validation(config, instance, expected_object, instance_id):
+        current_rs_type = get_current_rs_type(instance_id, config)
+        redisNum = get_redis_num(instance_id, config, current_rs_type)
+        redisId = get_shard_id(redisNum, 1)[0]
+
+        replicasetMaster = instance_id + "-master-" + current_rs_type
+        replicasetSlave = instance_id + "-slave-" + current_rs_type
+        masterName = replicasetMaster + "-" + str(redisId)
+        slaveName = replicasetSlave + "-" + str(redisId)
+        oldMasterRunTime = get_docker_running_time(config, instance_id, replicasetMaster, masterName)
+        oldSlaveRunTime = get_docker_running_time(config, instance_id, replicasetSlave, slaveName)
+
+        status = trigger_docker_failover("redis", config, instance_id, config["region"], docker_name=masterName)
+        assert status == 200
+        status = trigger_docker_failover("redis", config, instance_id, config["region"], docker_name=slaveName)
+        assert status == 200
+
+        assert wait_docker_run_time_change(config, instance_id, oldMasterRunTime, replicasetMaster, masterName)
+        assert wait_docker_run_time_change(config, instance_id, oldSlaveRunTime, replicasetSlave, slaveName)
+
+        # 等待手动运维恢复正常
+        for i in range(0, 3600):
+            if check_topo(instance_id, config) == 0:
+                break
+            time.sleep(1)
+
+        time.sleep(30)
+
+        assert check_admin_proxy_redis_configmap(instance_id, config, expected_object, instance["shardNumber"])
+
+    # 1个proxy、redis-master 同时发生failover
+    @staticmethod
+    def __master_proxy_failover_validation(config, instance, expected_object, instance_id):
+        current_rs_type = get_current_rs_type(instance_id, config)
+        redisNum = get_redis_num(instance_id, config, current_rs_type)
+        redisId = get_shard_id(redisNum, 1)[0]
+
+        replicasetMaster = instance_id + "-master-" + current_rs_type
+        masterName = replicasetMaster + "-" + str(redisId)
+        proxyId = get_shard_id(get_proxy_num(instance_id, config), 1)[0]
+        replicasetName = instance_id + "-proxy"
+        dockerName = replicasetName + "-" + str(proxyId)
+        oldMasterRunTime = get_docker_running_time(config, instance_id, replicasetMaster, masterName)
+        oldRunTime = get_docker_running_time(config, instance_id, replicasetName, dockerName)
+
+        status = trigger_docker_failover("redis", config, instance_id, config["region"], docker_name=masterName)
+        assert status == 200
+        status = trigger_docker_failover("proxy", config, instance_id, config["region"], id=proxyId)
+        assert status == 200
+
+        assert wait_docker_run_time_change(config, instance_id, oldMasterRunTime, replicasetMaster, masterName)
+        assert wait_docker_run_time_change(config, instance_id, oldRunTime, replicasetName, dockerName)
+
+        # 等待手动运维恢复正常
+        for i in range(0, 3600):
+            if check_topo(instance_id, config) == 0:
+                break
+            time.sleep(1)
+
+        time.sleep(30)
+        assert check_admin_proxy_redis_configmap(instance_id, config, expected_object, instance["shardNumber"])
+
+    # 1个proxy、redis-slave 同时发生failover
+    @staticmethod
+    def __slave_proxy_failover_validation(config, instance, expected_object, instance_id):
+        current_rs_type = get_current_rs_type(instance_id, config)
+        redisNum = get_redis_num(instance_id, config, current_rs_type)
+        redisId = get_shard_id(redisNum, 1)[0]
+
+        replicasetSlave = instance_id + "-slave-" + current_rs_type
+        slaveName = replicasetSlave + "-" + str(redisId)
+        proxyId = get_shard_id(get_proxy_num(instance_id, config), 1)[0]
+        replicasetName = instance_id + "-proxy"
+        dockerName = replicasetName + "-" + str(proxyId)
+        oldSlaveRunTime = get_docker_running_time(config, instance_id, replicasetSlave, slaveName)
+        oldRunTime = get_docker_running_time(config, instance_id, replicasetName, dockerName)
+
+        status = trigger_docker_failover("redis", config, instance_id, config["region"], docker_name=slaveName)
+        assert status == 200
+        status = trigger_docker_failover("proxy", config, instance_id, config["region"], id=proxyId)
+        assert status == 200
+
+        assert wait_docker_run_time_change(config, instance_id, oldSlaveRunTime, replicasetSlave, slaveName)
+        assert wait_docker_run_time_change(config, instance_id, oldRunTime, replicasetName, dockerName)
+
+        # 等待手动运维恢复正常
+        for i in range(0, 3600):
+            if check_topo(instance_id, config) == 0:
+                break
+            time.sleep(1)
+
+        time.sleep(30)
+        assert check_admin_proxy_redis_configmap(instance_id, config, expected_object, instance["shardNumber"])
+
+    # 1个proxy、redis-master、redis-slave同时发生failover
+    @staticmethod
+    def __slave_master_proxy_failover_validation(config, instance, expected_object, instance_id):
+        current_rs_type = get_current_rs_type(instance_id, config)
+        redisNum = get_redis_num(instance_id, config, current_rs_type)
+        redisId = get_shard_id(redisNum, 1)[0]
+
+        replicasetMaster = instance_id + "-master-" + current_rs_type
+        replicasetSlave = instance_id + "-slave-" + current_rs_type
+        masterName = replicasetMaster + "-" + str(redisId)
+        slaveName = replicasetSlave + "-" + str(redisId)
+        proxyId = get_shard_id(get_proxy_num(instance_id, config), 1)[0]
+        replicasetName = instance_id + "-proxy"
+        dockerName = replicasetName + "-" + str(proxyId)
+        oldMasterRunTime = get_docker_running_time(config, instance_id, replicasetMaster, masterName)
+        oldSlaveRunTime = get_docker_running_time(config, instance_id, replicasetSlave, slaveName)
+        oldRunTime = get_docker_running_time(config, instance_id, replicasetName, dockerName)
+
+        status = trigger_docker_failover("redis", config, instance_id, config["region"], docker_name=masterName)
+        assert status == 200
+        status = trigger_docker_failover("redis", config, instance_id, config["region"], docker_name=slaveName)
+        assert status == 200
+        status = trigger_docker_failover("proxy", config, instance_id, config["region"], id=proxyId)
+        assert status == 200
+
+        assert wait_docker_run_time_change(config, instance_id, oldMasterRunTime, replicasetMaster, masterName)
+        assert wait_docker_run_time_change(config, instance_id, oldSlaveRunTime, replicasetSlave, slaveName)
+        assert wait_docker_run_time_change(config, instance_id, oldRunTime, replicasetName, dockerName)
+
+        # 等待手动运维恢复正常
+        for i in range(0, 3600):
+            if check_topo(instance_id, config) == 0:
+                break
+            time.sleep(1)
+
+        time.sleep(30)
+        assert check_admin_proxy_redis_configmap(instance_id, config, expected_object, instance["shardNumber"])
+
+    # 所有节点同时发生failover
+    @staticmethod
+    def __all_nodes_failover_validation(config, instance, expected_object, instance_id):
+        current_rs_type = get_current_rs_type(instance_id, config)
+        redisNum = get_redis_num(instance_id, config, current_rs_type)
+        redisId = get_shard_id(redisNum, 1)[0]
+
+        replicasetMaster = instance_id + "-master-" + current_rs_type
+        replicasetSlave = instance_id + "-slave-" + current_rs_type
+        masterName = replicasetMaster + "-" + str(redisId)
+        slaveName = replicasetSlave + "-" + str(redisId)
+        replicasetadmin = instance_id + "-admin"
+        dockerAdmin = replicasetadmin + "-0"
+        oldAdminRunTime = get_docker_running_time(config, instance_id, replicasetadmin, dockerAdmin)
+        proxyId = get_shard_id(get_proxy_num(instance_id, config), 1)[0]
+        replicasetName = instance_id + "-proxy"
+        dockerName = replicasetName + "-" + str(proxyId)
+        oldMasterRunTime = get_docker_running_time(config, instance_id, replicasetMaster, masterName)
+        oldSlaveRunTime = get_docker_running_time(config, instance_id, replicasetSlave, slaveName)
+        oldRunTime = get_docker_running_time(config, instance_id, replicasetName, dockerName)
+
+        status = trigger_docker_failover("admin", config, instance_id, config["region"])
+        assert status == 200
+        status = trigger_docker_failover("redis", config, instance_id, config["region"], docker_name=masterName)
+        assert status == 200
+        status = trigger_docker_failover("redis", config, instance_id, config["region"], docker_name=slaveName)
+        assert status == 200
+        status = trigger_docker_failover("proxy", config, instance_id, config["region"], id=proxyId)
+        assert status == 200
+
+        assert wait_docker_run_time_change(config, instance_id, oldAdminRunTime, replicasetadmin, dockerAdmin)
+        assert wait_docker_run_time_change(config, instance_id, oldMasterRunTime, replicasetMaster, masterName)
+        assert wait_docker_run_time_change(config, instance_id, oldSlaveRunTime, replicasetSlave, slaveName)
+        assert wait_docker_run_time_change(config, instance_id, oldRunTime, replicasetName, dockerName)
+
+        # 等待手动运维恢复正常
+        for i in range(0, 3600):
+            if check_topo(instance_id, config) == 0:
+                break
+            time.sleep(1)
+
+        time.sleep(30)
+        assert check_admin_proxy_redis_configmap(instance_id, config, expected_object, instance["shardNumber"])
+
+    # 挑个1G主从版本即可
+
+    ## 有密码实例
+    # admin发生failover
+    # 单个proxy发生failover
+    # 单个Redis master 发生failover
+    # 单个redis slave 发生failover
+    # 1个redis-master、redis-slave 同时发生failover
+    # 1个proxy、redis-master 同时发生failover
+    # 1个proxy、redis-slave 同时发生failover
+    # 1个proxy、redis-master、redis-slave同时发生failover
+    # 所有节点同时发生failover
+
+    def test_cluster_failover_with_psw(self, config, instance_data, expected_data):
+        instance = instance_data["create_cluster_specified"][0]
+        expected_object = baseCheckPoint(expected_data[instance["cacheInstanceClass"]], instance["instance_password"])
+
+        client, resp, instance_id = create_instance_with_data(config, instance, expected_data)
+        self.__admin_failover_validation(config, instance, expected_object, instance_id)
+        self.__proxy_failover_validation(config, expected_object, instance_id)
+        self.__master_failover_validation(config, instance, expected_object, instance_id)
+        self.__slave_failover_validation(config, instance, expected_object, instance_id)
+        self.__master_proxy_failover_validation(config, instance, expected_object, instance_id)
+        self.__slave_proxy_failover_validation(config, instance, expected_object, instance_id)
+        self.__slave_master_proxy_failover_validation(config, instance, expected_object, instance_id)
+        self.__all_nodes_failover_validation(config, instance, expected_object, instance_id)
+
+        if instance_id is not None:
+            delete_instance(config, instance_id, client)
+
+    ## 无密码实例
+    # admin发生failover
+    # 单个proxy发生failover
+    # 单个Redis master 发生failover
+    # 单个redis slave 发生failover
+    # 1个redis-master、redis-slave 同时发生failover
+    # 1个proxy、redis-master 同时发生failover
+    # 1个proxy、redis-slave 同时发生failover
+    # 1个proxy、redis-master、redis-slave同时发生failover
+    # 所有节点同时发生failover
+    def test_cluster_failover_without_psw(self, config, instance_data, expected_data):
+        instance = instance_data["create_cluster_specified"][1]
+        expected_object = baseCheckPoint(expected_data[instance["cacheInstanceClass"]], instance["instance_password"])
+
+        client, resp, instance_id = create_instance_with_data(config, instance, expected_data)
+        self.__admin_failover_validation(config, instance, expected_object, instance_id)
+        self.__proxy_failover_validation(config, expected_object, instance_id)
+        self.__master_failover_validation(config, instance, expected_object, instance_id)
+        self.__slave_failover_validation(config, instance, expected_object, instance_id)
+        self.__master_proxy_failover_validation(config, instance, expected_object, instance_id)
+        self.__slave_proxy_failover_validation(config, instance, expected_object, instance_id)
+        self.__slave_master_proxy_failover_validation(config, instance, expected_object, instance_id)
+        self.__all_nodes_failover_validation(config, instance, expected_object, instance_id)
+
+        if instance_id is not None:
+            delete_instance(config, instance_id, client)
+
