3f2d3c1ae6ff26cd249a146d164093da96830c87
unknown <chenjing236@360buyad.local>
2020-04-30 17:49:13 +0800

    Fix test issue about create instance no billing

--- jmiss_redis_automation_test/steps/InstanceOperation.py
+++ jmiss_redis_automation_test/steps/InstanceOperation.py
@@ -42,6 +42,18 @@ def getHeader(conf, isErp=False):
 
     return header
 
+def getHeader_with_token(conf, isErp=False):
+    #测试环境header配置
+    header = {'x-jdcloud-pin': str(conf["user"]),
+              'x-jdcloud-redis-token': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJpb3QiLCJleHAiOjI1MjQ1NzkyMDAsImlhdCI6MTU4Nzg3MDM5MCwiaXNzIjoicmVkaXMiLCJzdWIiOiJiaWxsIn0.4m-dXMtFMbxgUaInvHTIVbOZDi1wyjTXy2hF1XkKDhw'}
+    #线上internal header配置
+    if str(conf["header"]) == "erp" or isErp:
+        header = {'x-jdcloud-pin': str(conf["user"]),
+                  'x-jdcloud-erp': 'duhaixing',
+                  'x-jdcloud-redis-token': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJpb3QiLCJleHAiOjI1MjQ1NzkyMDAsImlhdCI6MTU4Nzg3MDM5MCwiaXNzIjoicmVkaXMiLCJzdWIiOiJiaWxsIn0.4m-dXMtFMbxgUaInvHTIVbOZDi1wyjTXy2hF1XkKDhw'}
+
+    return header
+
 
 def client_send(client, req):
     start_time = time.time()
@@ -140,6 +152,7 @@ def create_instance_nobill(conf, data, redisVersion='4.0', ipv6On=None, shardNum
         assert False
     return client, resp, instance_id
 
+
 def create_validate_instance(config, instance_data,expected_object):
     client, resp, instance_id = create_instance_with_data(config, instance_data)
     instance = None
@@ -157,10 +170,13 @@ def create_validate_instance(config, instance_data,expected_object):
     return client, resp, instance_id
 
 
-def delete_instance(conf, instance_id, client=None):
+def delete_instance(conf, instance_id, client=None, token=False):
     if client is None:
         client = setClient(conf)
-    header = getHeader(conf)
+    if not token:
+        header = getHeader(conf)
+    else:
+        header = getHeader_with_token(conf)
     resp = None
     try:
         params = DeleteCacheInstanceParameters(str(conf["region"]), instance_id)
@@ -172,10 +188,28 @@ def delete_instance(conf, instance_id, client=None):
     return resp
 
 
-def query_instance(conf, instance_id, client=None):
+def query_instance(conf, instance_id, client=None, token=False):
     if client is None:
         client = setClient(conf)
-    header = getHeader(conf)
+    if not token:
+        header = getHeader(conf)
+    else:
+        header = getHeader_with_token(conf)
+    resp = None
+    try:
+        params = DescribeCacheInstanceParameters(str(conf["region"]), instance_id)
+        request = DescribeCacheInstanceRequest(params, header)
+        resp = client_send(client, request)
+    except Exception, e:
+        print e
+
+    return resp
+
+
+def query_instance_with_token(conf, instance_id, client=None):
+    if client is None:
+        client = setClient(conf)
+    header = getHeader_with_token(conf)
     resp = None
     try:
         params = DescribeCacheInstanceParameters(str(conf["region"]), instance_id)
@@ -226,14 +260,14 @@ def query_order_status(conf, request_id, client=None):
 def query_instance_by_name(name, conf):
     pass
 
-def query_instance_recurrent(wait_count, wait_time, instance_id, conf, client=None):
+def query_instance_recurrent(wait_count, wait_time, instance_id, conf, client=None, token=False):
     if client is None:
         client = setClient(conf)
     instance = None
     while wait_count > 0:
         print "---"+str(wait_count)+"---"
         time.sleep(wait_time)
-        resp = query_instance(conf, instance_id, client)
+        resp = query_instance(conf, instance_id, client, token)
         if resp is not None and resp.result is not None:
             if resp.result["cacheInstance"]["cacheInstanceStatus"] == "running":
                 instance = resp.result["cacheInstance"]
