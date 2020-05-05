188d443f5089c4e7642be87edbb4c929561201b2
root <root@A04-R018-I134-45-3000083.JCLOUD.COM>
2020-05-05 22:19:57 +0800

    Fix test issue of test describe spec config

--- jmiss_redis_automation_test/test_cases/openapi_cases/test_describeSpecConfig.py
+++ jmiss_redis_automation_test/test_cases/openapi_cases/test_describeSpecConfig.py
@@ -7,14 +7,14 @@ class TestDescribeSpecConfig:
     @pytest.mark.openapi
     @pytest.mark.regression
     @pytest.mark.cjdebug
-    def test_describe_spec_config(self, config):
+    def test_describe_spec_config(self, config, instance_data):
         resp = query_spec_conifg(config)
         assertRespNotNone(resp)
 
         specs = resp.result["instanceSpec"]["instanceVersions"][0]["instanceTypes"][0]["specs"]
-        assert(len(specs) == len(config["specs"]))
-        for spec in specs.sort():
-            result = filter(lambda x: x['instanceClass'] == spec["instanceClass"], config["specs"])
+        assert(len(specs) == len(instance_data["specs"]))
+        for spec in specs:
+            result = filter(lambda x: x['instanceClass'] == spec["instanceClass"], instance_data["specs"])
             isExists = list(result)
             assert(isExists)
 
