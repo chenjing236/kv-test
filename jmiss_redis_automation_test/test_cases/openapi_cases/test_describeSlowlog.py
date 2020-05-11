239108ca5768df7c912b9fdc577c33b87ed346c9
unknown <chenjing236@360buyad.local>
2020-05-11 14:34:38 +0800

    Modify slow log test case

--- jmiss_redis_automation_test/test_cases/openapi_cases/test_describeSlowlog.py
+++ jmiss_redis_automation_test/test_cases/openapi_cases/test_describeSlowlog.py
@@ -1,11 +1,11 @@
-from jmiss_redis_automation_test.steps.InstanceOperation import *
 from jmiss_redis_automation_test.steps.Valification import *
 from jmiss_redis_automation_test.steps.FusionOpertation import *
+from jmiss_redis_automation_test.steps.WebCommand import *
 
 
 class TestDescribeSlowLog:
     @pytest.mark.slowlog
-    def test_describeSlowlog(self, init_instance, config):
+    def test_describeSlowlog(self, init_instance, config, instance_data):
         client, resp, instance_id = init_instance
         resp = query_instance(config, instance_id, client)
         assertRespNotNone(resp)
@@ -13,9 +13,16 @@ class TestDescribeSlowLog:
         resp = query_slow_log(config, instance_id)
         assertRespNotNone(resp)
 
-        print("================")
+        print("=====set up slow log via instance config========")
         resp = set_config(config, instance_id, config["instance_config_slowlog"], client)
         assertRespNotNone(resp)
+
+        print("=======exec redis cli===========")
+        instance = instance_data["create_standard_specified"][0]
+        resp = send_web_command(config, instance_id, config["region"], "auth " + instance["instance_password"])
+        token = resp.result["token"]
+        object = WebCommand(config, instance_id, config["region"], token)
+        object.checkAllCommand()
+
         resp = query_slow_log(config, instance_id)
         assertRespNotNone(resp)
-        
