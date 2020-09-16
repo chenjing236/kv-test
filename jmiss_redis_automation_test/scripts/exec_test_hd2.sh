f36cbe58cc3e30f2e5dea1affb015dc63f629c66
chenjing236 <chenjing236@jd.com>
2020-09-16 16:30:23 +0800

    Update test env configuration

--- jmiss_redis_automation_test/scripts/exec_test_hd2.sh
+++ jmiss_redis_automation_test/scripts/exec_test_hd2.sh
@@ -2,5 +2,5 @@
 
 
 cd ../
-pytest ./test_cases/openapi_cases -s -v -m openapi --config ./config/conf_test_hd2.json --data ./data/instance_data_test_hd2.json
+pytest ./test_cases/openapi_cases -s -v -m openapi --config ./config/conf_test_hd2.json --data ./data/data_test.json
 
